78b040292141945d89a76f8ae458099e10779b07
Jon Edwards <jkedwards@me.com>
2015-04-07 15:14:43 -0700

    calculator works

--- client/index.js
+++ client/index.js
@@ -1,2 +1,42 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init() {
+  $('#equal').click(go);
+}
+
+function go() {
+  var number1 = $('#x').val();
+  var number2 = $('#y').val();
+  var op = $('#operator').val();
+
+  number1 = parseFloat(number1);
+  number2 = parseFloat(number2);
+
+  var result;
+  switch (op) {
+    case '+':
+      result = number1 + number2;
+      break;
+    case '-':
+      result = number1 - number2;
+      break;
+    case '*':
+      result = number1 * number2;
+      break;
+    case '/':
+      result = number1 / number2;
+  }
+  $('#result').val(result);
+}
+
+
+
+/*
+function crazy() {
+  setInterval(function(){
+    $('p').toggleClass('fun');
+  }, 2000);
+}
+*/
